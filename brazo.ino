e3d0945a1be7f33b0783b5f5342aa66be6ab6d38
Danielrayner <38916205+Danielrayner@users.noreply.github.com>
2018-05-02 22:51:42 -0500

    Posiciona inicial

---
+++ brazo.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+#define initBase 90
+#define initIzquierda 170
+#define initDerecha 20
+#define initGarra 0
+Servo Izquierda, Derecha, Garra, Base;
+
+void setup() 
+{
+ Base.attach(12);
+ Izquierda.attach(13);
+ Garra.attach(11);
+ Derecha.attach(10);
+ Izquierda.write(initIzquierda);
+ Derecha.write(initDerecha);
+ Base.write(initBase);
+ Garra.write(initGarra);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
