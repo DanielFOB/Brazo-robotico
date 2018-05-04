eaed1ccb191879bb41303b9da7632c9e9a237395
Danielrayner <38916205+Danielrayner@users.noreply.github.com>
2018-05-03 21:48:17 -0500

    Primera prueba de ciclos

--- brazo.ino
+++ brazo.ino
@@ -3,21 +3,69 @@
 #define initIzquierda 170
 #define initDerecha 20
 #define initGarra 0
+#define pinBase 12
+#define pinIzquierda 13
+#define pinDerecha 10
+#define pinGarra 11
 Servo Izquierda, Derecha, Garra, Base;
+int BanderaBase=0;
+int BanderaIzquierda=0;
+int BanderaDerecha=0;
+int BanderaGarra=0;
 
 void setup() 
 {
- Base.attach(12);
- Izquierda.attach(13);
- Garra.attach(11);
- Derecha.attach(10);
+ Base.attach(pinBase);
+ Izquierda.attach(pinIzquierda);
+ Garra.attach(pinGarra);
+ Derecha.attach(pinDerecha);
  Izquierda.write(initIzquierda);
  Derecha.write(initDerecha);
  Base.write(initBase);
  Garra.write(initGarra);
+ 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  if(BanderaBase==0)
+  {
+    
+    Base.write(Base.read()+1);
+    delay(20);
+    if(Base.read()==180)
+    {
+      BanderaDerecha=1;}}
+
+  if(BanderaDerecha==1)
+  {
+   
+ Derecha.write(Derecha.read()+1);
+    delay(20);
+    if(Derecha.read()==50)
+    {
+      BanderaGarra=1;}
+    }
+    if(BanderaGarra==1)
+  {
+   
+ Base.write(Base.read()-1);
+    delay(20);
+    if(Derecha.read()==0)
+    {
+      BanderaIzquierda=1;}
+    }
+      if(BanderaIzquierda==1)
+  {
+   
+ Derecha.write(Derecha.read()-1);
+    delay(20);
+    if(Derecha.read()==20)
+    {
+      BanderaBase=0;
+      BanderaDerecha=0;
+      BanderaGarra=0;
+      BanderaIzquierda=0;}
+    }
 
 }
