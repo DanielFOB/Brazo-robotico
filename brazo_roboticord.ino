0f93267151b40b4fd72b4c3327d99a21c4e02452
Danielrayner <38916205+Danielrayner@users.noreply.github.com>
2018-05-04 10:56:17 -0500

    Programa brazo pruebas de funcionamiento

--- brazo_roboticord.ino
+++ brazo_roboticord.ino
@@ -3,13 +3,13 @@
 // version 1.0
 // creado por: Rayner bermudez, Daniel oquendo
 //modificado por: Rayner bermudez, Daniel oquendo 
-// fecha de modificacion 04/05/2018
+// ultima modificacion 04/05/2018
 
 #include <Servo.h>
-#define initBase 90
-#define initIzquierda 170
-#define initDerecha 20
-#define initGarra 0
+#define initBase 90 //Posicion inicial de la base
+#define initIzquierda 80 //Posicion inicial del hombro iquierdo
+#define initDerecha 20 //Posicion inicial del hombro derecho
+#define initGarra 0 //Posicion inicial de la garra
 #define pinBase 12
 #define pinIzquierda 13
 #define pinDerecha 10
@@ -22,6 +22,7 @@ int BanderaGarra=0;
 
 void setup() 
 {
+//Posicion inicial del brazo.
  Base.attach(pinBase);
  Izquierda.attach(pinIzquierda);
  Garra.attach(pinGarra);
@@ -30,16 +31,18 @@ void setup()
  Derecha.write(initDerecha);
  Base.write(initBase);
  Garra.write(initGarra);
+ delay(2000);
  
 }
 
 void loop() 
 { 
+  //Mover hacia la izquierda o la derecha.
   if(BanderaBase==0)
   {
     
     Base.write(Base.read()-1);
-    delay(30);
+    delay(10);
     if(Base.read()==0)
     {
       BanderaBase=1;}}
@@ -48,16 +51,17 @@ if(BanderaBase==1)
   {
     
     Base.write(Base.read()+1);
-    delay(20);
+    delay(10);
     if(Base.read()==180)
     {
 
-      BanderaGarra=0;//aqui 
+      BanderaGarra=0;
+      //Abrir garra
       if(BanderaGarra==0)
   {
     
     Garra.write(Garra.read()+1);
-    delay(30);
+    delay(20);
     if(Garra.read()==5)
       {
         BanderaDerecha=0;
@@ -65,47 +69,76 @@ if(BanderaBase==1)
       }
       }}
       
-
+//Mover Base hacia derecha.
   if(BanderaDerecha==0)
   {
    
  Derecha.write(Derecha.read()+1);
-    delay(20);
+    delay(10);
     if(Derecha.read()==100)
     {
       
       BanderaDerecha=1;}
     }
+    //Subir Hombro Derecho.
     if(BanderaDerecha==1)
   {
    
    
  Derecha.write(Derecha.read()-1);
-    delay(20);
+    delay(10);
     if(Derecha.read()==20)
     {
       
       BanderaBase=2;}
     }
+    //Mover base hacia la izquierda.
        if(BanderaBase==2)
   {
     
     Base.write(Base.read()+1);
-    delay(30);
+    delay(10);
     if(Base.read()==180)
-    {
-      BanderaBase=3;}}
-
+    { 
+       BanderaBase=3;}}
+//Mover base hacia la derecha.
 if(BanderaBase==3)
   {
     
     Base.write(Base.read()-1);
-    delay(20);
+    delay(10);
     if(Base.read()==0)
     { 
-      BanderaBase=0;
+      BanderaIzquierda=0;
+      //Subir hombro izquierdo.
+if(BanderaIzquierda==0)
+  {
+    
+    Izquierda.write(Izquierda.read()+1);
+    delay(20);
+    if(Izquierda.read()==15)
+    {
+      BanderaIzquierda=0;}}
+     
+       BanderaBase=0;
       }
+      BanderaIzquierda=1;
+      //Bajar hombro izquierdo.
+      if(BanderaIzquierda==1)
+  {
+    
+    Izquierda.write(Izquierda.read()+1);
+    delay(20);
+    if(Izquierda.read()==20)
+    {
+      BanderaIzquierda=0;
+      BanderaBase=0;
+      }}
+
+      
     }
+    
+ 
     
 
 }
